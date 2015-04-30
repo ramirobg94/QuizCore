e8afe775b24b4e011e73e2ec8c452b4cea209b9c
ramirobg94 <ramiro.blazquezgon@alumnos.upm.es>
2015-04-30 13:33:53 +0200

    preguntas y respuestas

---
+++ controllers/quiz_controller.js
@@ -0,0 +1,14 @@
+//GEt /quizes/question
+exports.question = function(req,res) {
+	res.render('quizes/question' , {pregunta: 'Capital de Italia'});
+};
+
+//GEt /quizes/answer
+exports.answer = function(req,res) {
+	if(req.query.respuesta === 'Roma'){
+		res.render('quizes/answer',{respuesta: 'Correcto'});
+	} else{
+		res.render('quizes/answer',{respuesta: 'Incorrecto'});
+	}
+
+};
