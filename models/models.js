8f5214d9cafd5b35c63cc52020fda065d0450fcb
ramirobg94 <ramiro.blazquezgon@alumnos.upm.es>
2015-05-01 13:10:25 +0200

    paso7 DB

---
+++ models/models.js
@@ -0,0 +1,28 @@
+var path = require('path');
+
+//CARGAR modelo ORM
+var Sequelize = require('sequelize');
+
+//USAR BBDD SQlite
+var sequelize = new Sequelize(null,null,null,
+						{dialect: "sqlite", storage: "quiz.sqlite"}
+						);
+
+//Importar la definicion de la tabla quiz en quiz.js
+var Quiz = sequelize.import(path.join(__dirname,'quiz'));
+
+exports.Quiz = Quiz; //exportar definicion de la tabla Quiz
+
+//sequelize.sync() crea e inicializa la tabla de preguntas en DB
+sequelize.sync().success(function() {
+						)
+	//success(..) ejecuta el manejador una vez creada la tabla
+	Quiz.count().success( function(count){
+		if(count === 0) { //la tabla se inicializa solo si esta vacia
+			Quiz.create({ pregunta:'Capital de Italia',
+							respuesta: 'Roma
+							'})
+			.success( function(){console.log('Base de datos inicializa')});
+		};
+	});
+});
