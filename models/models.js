b360d11a8d6d5fcb324a95d2184f3cda4b57fd33
ramirobg94 <ramiro.blazquezgon@alumnos.upm.es>
2015-05-01 16:07:38 +0200

    presubida PG Heroku

--- models/models.js
+++ models/models.js
@@ -1,15 +1,35 @@
 var path = require('path');
 
+
+//Postgress DATABAS_URL = postgress://user:passwd@host:port/database
+//Sqlite DATABASE_URL = sqlite://:@:/
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name = (url[6] || null);
+var user  = (url[2] || null);
+var pwd  = (url[3] || null);
+var protocol  = (url[1] || null);
+var dialect = (url[1] || null);
+var port = (url[5] || null);
+var host = (url[4] || null);
+var storage = process.env.DATABASE_STORAGeE;
+
 //CARGAR modelo ORM
 var Sequelize = require('sequelize');
 
-//USAR BBDD SQlite
-var sequelize = new Sequelize(null,null,null,
-						{dialect: "sqlite", storage: "quiz.sqlite"}
-						);
+//USAR BBDD SQlite o  Postgres
+var sequelize = new Sequelize(DB_name,user,pwd,
+						{dialect: protocol,
+						protocol: protocol,
+						port: port,
+						host: host, 
+						storage: storage,
+						omitNull: true
+					}
+				);
 
 //Importar la definicion de la tabla quiz en quiz.js
-var Quiz = sequelize.import(path.join(__dirname,'quiz'));
+var quiz_path = path.join(__dirname,'quiz');
+var Quiz = sequelize.import(quiz_path);
 
 exports.Quiz = Quiz; //exportar definicion de la tabla Quiz
 
