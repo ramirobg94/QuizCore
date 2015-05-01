a8e18588feb0413d8a1874c9c5d51b5f22046dd9
ramirobg94 <ramiro.blazquezgon@alumnos.upm.es>
2015-05-01 13:31:26 +0200

    Despliegue DB en Heroku

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,15 +1,15 @@
-var models = require('../models/models.js')
+var models = require('../models/models.js');
 
 //GEt /quizes/question
 exports.question = function(req,res) {
-	models.Quiz.findAll().success(function(quiz){
-		es.render('quizes/question' , {pregunta: quiz[0].pregunta});
+	models.Quiz.findAll().then(function(quiz){
+		res.render('quizes/question' , {pregunta: quiz[0].pregunta});
 	})
 };
 
 //GEt /quizes/answer
 exports.answer = function(req,res) {
-	models.Quiz.findAll().success(function(quiz){
+	models.Quiz.findAll().then(function(quiz){
 		if(req.query.respuesta === quiz[0].respuesta){
 			res.render('quizes/answer',{respuesta: 'Correcto'});
 		} else{
--- models/models.js
+++ models/models.js
@@ -14,15 +14,14 @@ var Quiz = sequelize.import(path.join(__dirname,'quiz'));
 exports.Quiz = Quiz; //exportar definicion de la tabla Quiz
 
 //sequelize.sync() crea e inicializa la tabla de preguntas en DB
-sequelize.sync().success(function() {
-						)
-	//success(..) ejecuta el manejador una vez creada la tabla
-	Quiz.count().success( function(count){
+sequelize.sync().then(function() {
+	//then(..) ejecuta el manejador una vez creada la tabla
+	Quiz.count().then(function(count){
 		if(count === 0) { //la tabla se inicializa solo si esta vacia
 			Quiz.create({ pregunta:'Capital de Italia',
-							respuesta: 'Roma
-							'})
-			.success( function(){console.log('Base de datos inicializa')});
+							respuesta: 'Roma'
+						})
+			.then( function(){console.log('Base de datos inicializa')});
 		};
 	});
 });
