b33ee948667cad5d7895bae7759460d1c1fbf529
ramirobg94 <ramiro.blazquezgon@alumnos.upm.es>
2015-05-02 19:52:35 +0200

    Crear preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -3,6 +3,30 @@ var por = '%';
 var search;
 var misearch;
 
+//GET /quizes/new
+exports.new = function(req,res){
+	var quiz = models.Quiz.build( //Crea un objeto Quiz
+		{pregunta: "Pregunta", respuesta: "Respuesta"}
+		);
+
+	res.render('quizes/new', {quiz: quiz});
+};
+// POST /quizes/create
+exports.create = function(req, res) {
+	var quiz = models.Quiz.build( req.body.quiz);
+
+	if((req.body.quiz.pregunta === "") || (req.body.quiz.respuesta === "")){
+		res.redirect('/quizes')
+	}else{
+
+	//Guarda en la DB los campos pregunta y respuesta de quiz
+	quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
+		res.redirect('/quizes');
+
+	}) //REdireccion HTTP (URL relativo) lista e preguntas
+	}
+};
+
 //Autoload - factoriza el codigo si ruta incluye :quizId
 exports.load = function(req, res, next, quizId){
 	models.Quiz.find(quizId).then(
@@ -49,5 +73,5 @@ exports.answer = function(req,res) {
 		if(req.query.respuesta.toLowerCase() === req.quiz.respuesta.toLowerCase()){
 			resultado = 'Correcto';
 		} 
-		res.render('quizes/answer',	{quiz: req.quiz, respuesta: resultado, a: req.quiz.respuesta, b: req.query.respuesta});
+		res.render('quizes/answer',	{quiz: req.quiz, respuesta: resultado});
 };
