53ef4ffb52f65b01ddc2bef6ba22ca613719fea9
ramirobg94 <ramiro.blazquezgon@alumnos.upm.es>
2015-05-02 03:18:42 +0200

    multiples preguntas

--- models/models.js
+++ models/models.js
@@ -43,6 +43,9 @@ sequelize.sync().then(function() {
 						});
 			Quiz.create({ pregunta:'Capital de Portugal',
 							respuesta: 'Lisboa'
+						});
+			Quiz.create({ pregunta:'Capital de España',
+							respuesta: 'Madrid'
 						})
 			.then( function(){console.log('Base de datos inicializa')});
 		};
