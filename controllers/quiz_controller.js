15afee9cf5a685c4604edaa1d6811db8459ecfb9
ramirobg94 <ramiro.blazquezgon@alumnos.upm.es>
2015-05-03 03:55:53 +0200

    borrar a falta de mejoras visuales

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -51,6 +51,12 @@ exports.update = function(req, res){
 );
 };
 
+//DELETE /quizes/:id
+exports.destroy = function(req,res){
+	req.quiz.destroy().then(function(){
+		res.redirect('/quizes');
+	}).catch(function(error){next(error)});
+};
 
 //Autoload - factoriza el codigo si ruta incluye :quizId
 exports.load = function(req, res, next, quizId){
