f65f7fbc82fbb958f8b8a84a18ab13e7ce7297a9
ramirobg94 <ramiro.blazquezgon@alumnos.upm.es>
2015-05-03 13:02:14 +0200

    prueba

--- models/models.js
+++ models/models.js
@@ -1,5 +1,5 @@
 var path = require('path');
-
+var pg = require('pg');
 
 //Postgress DATABAS_URL = postgress://user:passwd@host:port/database
 //Sqlite DATABASE_URL = sqlite://:@:/
@@ -8,7 +8,7 @@ var DB_name = (url[6] || null);
 var user  = (url[2] || null);
 var pwd  = (url[3] || null);
 var protocol  = (url[1] || null);
-var dialect = "sqlite";
+var dialect = (url[1] || null);
 var port = (url[5] || null);
 var host = (url[4] || null);
 var storage = process.env.DATABASE_STORAGE;
